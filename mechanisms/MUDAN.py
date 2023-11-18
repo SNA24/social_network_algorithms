d852ddffc38122ddc8f45ba025a0f88492e55b6d
graziaFerrara <grazia.ferrara2000@gmail.com>
2023-11-18 18:58:44 +0100

    updated files

--- mechanisms/MUDAN.py
+++ mechanisms/MUDAN.py
@@ -1,6 +1,6 @@
 import random
 
-def mudan_m(k, seller_net, reports, bids):
+def mudan(k, seller_net, reports, bids):
     """
     # Parameters
     k : int
@@ -26,7 +26,7 @@ def mudan_m(k, seller_net, reports, bids):
         means that the bidder is paying to the seller, while a negative price means that 
         the seller is paying to the bidder.
     """
-    # Implement the MUDAN-m mechanism based on Fang et al. (2023) or your specific algorithm
+    # Implement the MUDAN mechanism based on Fang et al. (2023) or your specific algorithm
 
     allocation = { bidder : False for bidder in bids.keys() }
     payments = { bidder : 0 for bidder in bids.keys() }
@@ -39,43 +39,45 @@ def mudan_m(k, seller_net, reports, bids):
 
     W = set() # winner set
     A = reports[seller] if seller in reports.keys() else set() 
+    marked = set() # marked set
     if len(A) == 0:
         return allocation, payments
     # init P, potential winner set
     if len(A.difference(W)) <= k:
         P = A
     else:
-        # P contains the top k elements of A/W in terms of bids
-        P = set([item[0] for item in sorted(bids.items(), key=lambda x: x[1], reverse=True) if item[0] in A.difference(W)][0:k])
+        # P := W ∪ {i_1,...,i_m′}
+        P = W.union(set([item[0] for item in sorted(bids.items(), key=lambda x: x[1], reverse=True) if item[0] in A.difference(W)][0:k]))
 
     # while the difference between P and W is different from the empty set, do
     while len(P.difference(W)) > 0:
         # while A contains an unmarked agent i \in W \union (A \difference P), do
-        unmarked_agents = A.intersection(W.union(A.difference(P)))
+        unmarked_agents = A.intersection(W.union(A.difference(P))).difference(marked)
 
         for unmarked_agent in unmarked_agents:
             # update A with A \union r_i', mark the agent i 
             if unmarked_agent in reports.keys():
                 A = A.union(reports[unmarked_agent])
+            marked.add(unmarked_agent)
 
             # update P
             if len(A.difference(W)) <= k:
                 P = A
             else:
-                # P contains the top k elements of A/W in terms of bids
-                P = set([item[0] for item in sorted(bids.items(), key=lambda x: x[1], reverse=True) if item[0] in A.difference(W)][0:k])
+                # P := W ∪ {i_1,...,i_m′}
+                P = W.union(set([item[0] for item in sorted(bids.items(), key=lambda x: x[1], reverse=True) if item[0] in A.difference(W)][0:k]))
 
         # assign a priority \sigma to each agent in P
         sigma = { agent: len(reports[agent]) if agent in reports.keys() else 0 for agent in P }
         diff_PW = P.difference(W)
+        diff_AW = A.difference(W)
         # take the w elementi in siff_PW with the highest priority in W
         w = sorted(diff_PW, key=lambda x: sigma[x], reverse=True)[0]
         W.add(w[0])
         # record the tentative payment of w
-        diff_AW = A.difference(W)
         sorted_valuation = sorted([bids[agent] for agent in diff_AW], reverse=True)
-        if len(sorted_valuation) > k-1:
-            tentative_payments[w] = sorted_valuation[k-1]
+        if len(sorted_valuation) > k:
+            tentative_payments[w] = sorted_valuation[k]
         else:
             tentative_payments[w] = 0
         k -= 1
@@ -99,6 +101,6 @@ if __name__ == '__main__':
 
     bids = {'a': 3, 'b': 1, 'c': 1, 'd': 6, 'e': 4, 'f': 7, 'g': 5}
 
-    allocation, payments = mudan_m(k, seller_net, reports, bids)
+    allocation, payments = mudan(k, seller_net, reports, bids)
     print(allocation)
     print(payments)
