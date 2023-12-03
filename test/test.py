7d05ba622ccf92cf2b7ddede90db150b94505060
graziaFerrara <grazia.ferrara2000@gmail.com>
2023-12-03 17:43:00 +0100

    large networks to test

--- test/test.py
+++ test/test.py
@@ -25,48 +25,48 @@ from centrality_measures.HITS import hits_both, parallel_hits_both, hits_authori
 to_test_mining = {
     # diameter: False,
     # parallel_diam: True,
-    # stream_diam: False,
+    stream_diam: False,
     num_triangles: False,
     parallel_num_triangles: True,
-    # girvan_newman: False,
-    # heuristic_girvan_newman: False,
-    # parallel_girvan_newman: True,
-    # parallel_heuristic_girvan_newman: True,
-    # spectral: False,
-    # spectral_parallel: True,
-    # spectral_multi_cluster: False,
-    # spectral_multi_cluster_parallel: True,
-    # spectral_multi_cluster_v2: False,
-    # spectral_multi_cluster_v2_parallel: True,
-    # hierarchical: False,
-    # parallel_hierarchical: True,
-    # two_means: False,
-    # parallel_two_means: True
+    girvan_newman: False,
+    heuristic_girvan_newman: False,
+    parallel_girvan_newman: True,
+    parallel_heuristic_girvan_newman: True,
+    spectral: False,
+    spectral_parallel: True,
+    spectral_multi_cluster: False,
+    spectral_multi_cluster_parallel: True,
+    spectral_multi_cluster_v2: False,
+    spectral_multi_cluster_v2_parallel: True,
+    hierarchical: False,
+    parallel_hierarchical: True,
+    two_means: False,
+    parallel_two_means: True
 }
 
 to_test_cenetrality_measures = {
-    # betweenness: False,
-    # parallel_betweenness: True,
-    # closeness: False,
-    # parallel_closeness: True,
-    # degree: False,
-    # parallel_degree: True,
-    # shapley_degree: False,
-    # parallel_shapley_degree: True,
-    # shapley_threshold: False,
-    # parallel_shapley_threshold: True,
-    # shapley_closeness: False,
-    # parallel_shapley_closeness: True,
-    # vote_rank: False,
-    # parallel_vote_rank: True,
-    # page_rank: False,
-    # parallel_page_rank: True,
-    # hits_both: False,
-    # parallel_hits_both: True,
-    # hits_authority: False,
-    # parallel_hits_authority: True,
-    # hits_hubbiness: False,
-    # parallel_hits_hubbiness: True
+    betweenness: False,
+    parallel_betweenness: True,
+    closeness: False,
+    parallel_closeness: True,
+    degree: False,
+    parallel_degree: True,
+    shapley_degree: False,
+    parallel_shapley_degree: True,
+    shapley_threshold: False,
+    parallel_shapley_threshold: True,
+    shapley_closeness: False,
+    parallel_shapley_closeness: True,
+    vote_rank: False,
+    parallel_vote_rank: True,
+    page_rank: False,
+    parallel_page_rank: True,
+    hits_both: False,
+    parallel_hits_both: True,
+    hits_authority: False,
+    parallel_hits_authority: True,
+    hits_hubbiness: False,
+    parallel_hits_hubbiness: True
 }
 
 def test_network(G, name, to_test, topic):
@@ -79,12 +79,12 @@ def test_network(G, name, to_test, topic):
         if parallel:
             for j in num_jobs:
                 start = time.time()
-                print(name, func.__name__, func(G, j), j)
+                name, func.__name__, func(G, j), j
                 end = time.time()
                 dataframe = dataframe._append({'function': func.__name__, 'num_jobs': j, 'execution_time': end - start}, ignore_index=True)
         else:
             start = time.time()
-            print(name, func.__name__, func(G))
+            name, func.__name__, func(G)
             end = time.time()
             dataframe = dataframe._append({'function': func.__name__, 'num_jobs': 1, 'execution_time': end - start}, ignore_index=True)
         
@@ -122,10 +122,32 @@ if __name__ == '__main__':
             to_node = line[1]
             moreno2.add_edge(from_node, to_node)
 
+    test_directed = nx.DiGraph()
+    with open('test/citation-net/Cit-HepTh.txt') as f:
+        lines = f.readlines()
+        lines = lines[4:]
+        for line in lines:
+            line = line.strip().split('\t')
+            from_node = line[0]
+            to_node = line[1]
+            test_directed.add_edge(from_node, to_node)
+
+    test_undirected = nx.Graph()
+    with open('test/facebook-large/musae_facebook_edges.csv') as f:
+        lines = f.readlines()
+        lines = lines[1:]
+        for line in lines:
+            line = line.strip().split(',')
+            from_node = line[0]
+            to_node = line[1]
+            test_undirected.add_edge(from_node, to_node)
+
     graphs = {
-        'zachary': zachary,
-        'moreno': moreno,
-        'moreno2': moreno2
+        # 'zachary': zachary,
+        # 'moreno': moreno,
+        # 'moreno2': moreno2,
+        # 'test_directed': test_directed,
+        'test_undirected': test_undirected
     }
 
     for name, G in graphs.items():
